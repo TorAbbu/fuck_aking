8e8aff75994910c0aac46b88f935fa5552100f01
TorAbbu <89208996+TorAbbu@users.noreply.github.com>
2022-07-03 10:42:22 +0600

    Update akingfuck.py

--- akingfuck.py
+++ akingfuck.py
@@ -9,7 +9,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations ! Your Device Support this Tools")
 
-    from fuck import Subscription
+ 
     Subscription(0)
 elif bit == '32bit':
     print("\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools")
